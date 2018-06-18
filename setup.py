ebc094696ef99fc7c44310331b9189e0cd56585f
liyatao <841483350@qq.com>
2018-06-18 10:25:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #coding:utf8
 from distutils.core import setup
 import py2exe
+setup(console=["weiyuanchuang.py"])
+#setup(windows=["weiyuanchuang.py"],options = { "py2exe":{"dll_excludes":["MSVCP90.dll"]}})  #编译为WINDOWS  GUI程序
 
-setup(console=['baidu_xiangguan_keyword.py'])
